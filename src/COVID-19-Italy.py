dcd218340e650dea441c1a58c2626c9a70e1b177
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-03-15 20:01:03 +0100

    Small stylistic improvements

--- src/COVID-19-Italy.py
+++ src/COVID-19-Italy.py
@@ -14,7 +14,9 @@ if language == "English":
     st.sidebar.markdown(
         "Choose if you prefer to visualise the total of the chosen indicator or the day-to-day increment or decrement:"
     )
-    data_rate = st.sidebar.radio(label="Available visualisations", options=["total", "day-to-day"])
+    data_rate = st.sidebar.radio(
+        label="Available visualisations", options=["total", "day-to-day"]
+    )
     english_line_plots(data, mode=data_rate)
 
 elif language == "Italiano":
@@ -22,8 +24,12 @@ elif language == "Italiano":
     st.sidebar.markdown(
         "Scegli se preferisci visualizzare il dato totale o il relativo cambiamento rispetto al giorno precedente:"
     )
-    data_rate = st.sidebar.radio(label="Possibili visualizzazioni", options=["totale", "giorno per giorno"])
+    data_rate = st.sidebar.radio(
+        label="Possibili visualizzazioni", options=["totale", "giorno per giorno"]
+    )
     mode = "total" if data_rate == "totale" else "day-to-day"
     italian_line_plots(data, mode=mode)
 
-st.sidebar.title("To contribute or view the code, please see [github](https://github.com/tommasobonomo/covid19-italy)")
+st.sidebar.title(
+    "To contribute or view the code, please see [github](https://github.com/tommasobonomo/covid19-italy)"
+)
