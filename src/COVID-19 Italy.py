7a29e2ad307fe62da482e3ab3d5745ea97bbb9c7
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-03-15 13:02:26 +0100

    Finalised sidebar with language and mode

--- src/COVID-19 Italy.py
+++ src/COVID-19 Italy.py
@@ -1,5 +1,30 @@
-from line_plots import italian_line_plots
+import streamlit as st
+
+import line_plots
 from utils import get_data
 
 data = get_data()
-italian_line_plots(data)
+
+language = st.sidebar.radio(label="", options=["English", "Italiano"])
+
+if language == "English":
+    st.sidebar.markdown("# Possible visualisations")
+    st.sidebar.markdown(
+        "Choose if you prefer to visualise the total of the chosen indicator or the day-to-day increment or decrement:"
+    )
+    data_rate = st.sidebar.radio(
+        label="Possibili visualisations", options=["total", "day-to-day"]
+    )
+    line_plots.english_line_plots(data, mode=data_rate)
+
+elif language == "Italiano":
+    st.sidebar.markdown("# Possibili visualizzazioni")
+    st.sidebar.markdown(
+        "Scegli se preferisci visualizzare il dato totale o il relativo cambiamento rispetto al giorno precedente:"
+    )
+    data_rate = st.sidebar.radio(
+        label="Possibili visualizzazioni", options=["totale", "giorno per giorno"]
+    )
+    mode = "total" if data_rate == "totale" else "day-to-day"
+    line_plots.italian_line_plots(data, mode=mode)
+
