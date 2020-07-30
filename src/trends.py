dc0b67cdae8588e3da0197200bd954c676cc8b25
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-07-30 10:09:51 +0200

    Replace growth factor with weekly averages (#18)
    * Fix default value of date selector in maps
    
    * Minor updates
    
    - added .venv to .gitignore
    - fixed a linting error
    - updated some defaults in plots
    
    * Implemented average plot, for any feature
    
    * Updated localisation for averaging trends feature

--- src/trends.py
+++ src/trends.py
@@ -5,10 +5,9 @@ import streamlit as st
 from gettext import NullTranslations
 
 from utils import (
+    average_over_days,
     get_features,
     formatter,
-    calculate_growth_factor,
-    regional_growth_factor,
     generate_global_chart,
     generate_regional_chart,
 )
@@ -27,10 +26,7 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
     )
 
     # Group data by date
-    gf_prefix = _("growth_factor")
-    growth_scale = alt.Scale(type="linear")
     general = data.groupby("data", as_index=False).sum()
-    general = calculate_growth_factor(general, features, prefix=gf_prefix)
 
     # Choose log scale or linear, defines what feature to use
     general_choice = st.radio(label=_("Scale"), options=[_("linear"), _("logarithmic")])
@@ -41,30 +37,28 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
         general_scale = alt.Scale(type="linear")
 
     st.markdown(("## " + _("General data")))
-    general_chart = generate_global_chart(
-        general, feature, general_scale, _("Month and day")
-    )
-    st.altair_chart(general_chart)
 
-    st.markdown(("### " + _("Growth factor")))
-    st.markdown(
-        _(
-            """
-        The growth factor is the multiplier of the exponential growth curve, calculated as:
-        $$
-        \\frac{cases_{n+1}}{cases_{n}}
-        $$
-        where $cases_n$ stands for the number of cases registered up to and including day $n$. For example,
-        if the total number of cases registered was 300 yesterday and 400 today, the growth factor would be
-        1.33, as $\\frac{400}{300} = 1.33$.
-        """
+    is_general_average = st.checkbox(label=_("Average over days"), key="avg1")
+    if is_general_average:
+        avg_days = st.slider(
+            label=_("Days to average over"),
+            min_value=2,
+            max_value=21,
+            value=7,
+            key="slider1",
         )
-    )
-
-    growth_chart = generate_global_chart(
-        general, f"{gf_prefix}_{feature}", growth_scale, _("Month and day")
-    )
-    st.altair_chart(growth_chart)
+        general_average = average_over_days(
+            general[[feature, "data"]], categorical_columns=["data"], avg_days=avg_days
+        )
+        general_average_chart = generate_global_chart(
+            general_average, feature, general_scale, _("Month and day")
+        )
+        st.altair_chart(general_average_chart)
+    else:
+        general_chart = generate_global_chart(
+            general, feature, general_scale, _("Month and day")
+        )
+        st.altair_chart(general_chart)
 
     st.markdown(("## " + _("Situation in different regions")))
 
@@ -81,7 +75,6 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
     if selected_regions.empty:
         st.warning(_("No region selected!"))
     else:
-        selected_regions = regional_growth_factor(selected_regions, features, gf_prefix)
         regional_choice = st.radio(
             label=_("Regional Scale"), options=[_("linear"), _("logarithmic")]
         )
@@ -91,23 +84,42 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
         else:
             regional_scale = alt.Scale(type="linear")
 
-        st.markdown(("### " + _("General data")))
-        regional_chart = generate_regional_chart(
-            selected_regions,
-            feature,
-            regional_scale,
-            x_title=_("Month and day"),
-            color_title=_("Region"),
-        )
-        st.altair_chart(regional_chart)
+        is_regional_average = st.checkbox(label=_("Average over days"), key="avg2")
+        if is_regional_average:
+            avg_days = st.slider(
+                label=_("Days to average over"),
+                min_value=2,
+                max_value=21,
+                value=7,
+                key="slider2",
+            )
+            regional_average = (
+                selected_regions.groupby(["denominazione_regione"], as_index=False)
+                .apply(
+                    lambda group: average_over_days(
+                        group[[feature, "data", _("denominazione_regione")]],
+                        ["data", "denominazione_regione"],
+                        avg_days,
+                    )
+                )
+                .reset_index(level=0, drop=True)
+                .reset_index(drop=True)
+            )
 
-        st.markdown(("### " + _("Growth factor")))
-        regional_growth_chart = generate_regional_chart(
-            selected_regions,
-            f"{gf_prefix}_{feature}",
-            growth_scale,
-            x_title=_("Month and day"),
-            color_title=_("Region"),
-            legend_position="bottom-left",
-        )
-        st.altair_chart(regional_growth_chart)
+            regional_average_chart = generate_regional_chart(
+                regional_average,
+                feature,
+                regional_scale,
+                x_title=_("Month and day"),
+                color_title=_("Region"),
+            )
+            st.altair_chart(regional_average_chart)
+        else:
+            regional_chart = generate_regional_chart(
+                selected_regions,
+                feature,
+                regional_scale,
+                x_title=_("Month and day"),
+                color_title=_("Region"),
+            )
+            st.altair_chart(regional_chart)
