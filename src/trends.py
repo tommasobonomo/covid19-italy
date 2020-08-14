f9ee8fca0b4415af041abe99bcc1696735182b8f
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-08-14 11:34:32 +0200

    Small enhancements (#20)

--- src/trends.py
+++ src/trends.py
@@ -1,3 +1,4 @@
+import datetime
 import altair as alt
 import pandas as pd
 import streamlit as st
@@ -17,16 +18,30 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
     """Renders line plots, both general and regional, of data argument. Usually it is the resulting DataFrame from the website of the Protezione civile."""
     _ = lang.gettext
 
+    # Group data by date
+    general = data.groupby("data", as_index=False).sum()
+
     st.title(_("COVID-19 in Italy - Temporal trend"))
+
+    st.markdown("### " + _("14-day cases per 100.000:"))
+
+    now = datetime.datetime.now()
+
+    if now.hour >= 18:
+        today = now.date()
+    else:
+        today = now.date() - datetime.timedelta(days=1)
+
+    fourteen_day_new_positves = general[
+        today - datetime.timedelta(days=14) < general["data"]
+    ][_("new_positive")]
+    st.write(float(f"{fourteen_day_new_positves.sum() * 100000 / 60360000:.2f}"))
 
     st.markdown(_("What indicator would you like to visualise?"))
     features = get_features(data)
     feature = st.selectbox(
-        label=_("Choose..."), options=features, format_func=formatter, index=11
+        label=_("Choose..."), options=features, format_func=formatter, index=6
     )
-
-    # Group data by date
-    general = data.groupby("data", as_index=False).sum()
 
     # Choose log scale or linear, defines what feature to use
     general_choice = st.radio(label=_("Scale"), options=[_("linear"), _("logarithmic")])
@@ -38,7 +53,9 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
 
     st.markdown(("## " + _("General data")))
 
-    is_general_average = st.checkbox(label=_("Average over days"), key="avg1")
+    is_general_average = st.checkbox(
+        label=_("Average over days"), key="avg1", value=True
+    )
     if is_general_average:
         avg_days = st.slider(
             label=_("Days to average over"),
@@ -50,15 +67,21 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
         general_average = average_over_days(
             general[[feature, "data"]], categorical_columns=["data"], avg_days=avg_days
         )
-        general_average_chart = generate_global_chart(
-            general_average, feature, general_scale, _("Month and day")
-        )
-        st.altair_chart(general_average_chart)
-    else:
-        general_chart = generate_global_chart(
-            general, feature, general_scale, _("Month and day")
-        )
-        st.altair_chart(general_chart)
+
+        general = general_average
+
+    general_chart = generate_global_chart(
+        general, feature, general_scale, _("Month and day")
+    )
+    st.altair_chart(general_chart)
+
+    todays_latest = general[general["data"] == today][feature].iloc[0]
+
+    st.markdown(
+        _("Latest data on ")
+        + f"**{formatter(feature).lower()}**: "
+        + f"{todays_latest:.2f}"
+    )
 
     st.markdown(("## " + _("Situation in different regions")))
 
@@ -84,7 +107,9 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
         else:
             regional_scale = alt.Scale(type="linear")
 
-        is_regional_average = st.checkbox(label=_("Average over days"), key="avg2")
+        is_regional_average = st.checkbox(
+            label=_("Average over days"), key="avg2", value=True
+        )
         if is_regional_average:
             avg_days = st.slider(
                 label=_("Days to average over"),
