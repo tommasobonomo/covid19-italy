e797584df8c8f359ed5d3d2a7e00e843f8261c63
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-08-18 15:53:24 +0200

    Add daily diff for any indicator (#22)

--- src/trends.py
+++ src/trends.py
@@ -7,6 +7,7 @@ from gettext import NullTranslations
 
 from utils import (
     average_over_days,
+    diff_over_previous_datapoint,
     get_features,
     formatter,
     generate_global_chart,
@@ -25,23 +26,35 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
 
     st.markdown("### " + _("14-day cases per 100.000:"))
 
+    # Get today
     now = datetime.datetime.now()
-
     if now.hour >= 18:
         today = now.date()
     else:
         today = now.date() - datetime.timedelta(days=1)
 
+    # Filter for most recent 14 days and write calculation
     fourteen_day_new_positves = general[
         today - datetime.timedelta(days=14) < general["data"]
     ][_("new_positive")]
-    st.write(float(f"{fourteen_day_new_positves.sum() * 100000 / 60360000:.2f}"))
+    st.write(float(f"{fourteen_day_new_positves.sum() * 100000 / 60450414:.2f}"))
 
+    # Indicator chooser
     st.markdown(_("What indicator would you like to visualise?"))
     features = get_features(data)
     feature = st.selectbox(
         label=_("Choose..."), options=features, format_func=formatter, index=6
     )
+
+    # Add checkbox for diff with most recent data for an indicator
+    diff = st.checkbox(label=_("Difference with previous datapoint"))
+    st.markdown(
+        _(
+            "By checking the above box, the indicator will be replaced by the difference of its value between two consecutive days. This helps in untangling cumulative data such as deaths and total tests."
+        )
+    )
+    if diff:
+        general = diff_over_previous_datapoint(general, "data", feature)
 
     # Choose log scale or linear, defines what feature to use
     general_choice = st.radio(label=_("Scale"), options=[_("linear"), _("logarithmic")])
@@ -53,6 +66,7 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
 
     st.markdown(("## " + _("General data")))
 
+    # Average calculation if needed
     is_general_average = st.checkbox(
         label=_("Average over days"), key="avg1", value=True
     )
@@ -86,18 +100,32 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
     st.markdown(("## " + _("Situation in different regions")))
 
     # Get list of regions and select the ones of interest
-    region_options = data["denominazione_regione"].sort_values().unique().tolist()
+    region_options = data[_("denominazione_regione")].sort_values().unique().tolist()
     regions = st.multiselect(
         label=_("Regions"),
         options=region_options,
         default=["Lombardia", "Veneto", "Emilia-Romagna"],
     )
     # Filter regions in selection
-    selected_regions = data[data["denominazione_regione"].isin(regions)]
+    selected_regions = data[data[_("denominazione_regione")].isin(regions)][
+        ["data", _("denominazione_regione"), feature]
+    ]
 
     if selected_regions.empty:
         st.warning(_("No region selected!"))
     else:
+
+        if diff:
+            selected_regions = (
+                selected_regions.groupby([_("denominazione_regione")])
+                .apply(
+                    lambda group: diff_over_previous_datapoint(group, "data", feature)
+                )
+                .sort_values(by="data", ascending=True)
+                .reset_index(level=0, drop=True)
+                .reset_index(drop=True)
+            )
+
         regional_choice = st.radio(
             label=_("Regional Scale"), options=[_("linear"), _("logarithmic")]
         )
@@ -119,11 +147,11 @@ def line_plots(data: pd.DataFrame, lang: NullTranslations) -> None:
                 key="slider2",
             )
             regional_average = (
-                selected_regions.groupby(["denominazione_regione"], as_index=False)
+                selected_regions.groupby([_("denominazione_regione")], as_index=False)
                 .apply(
                     lambda group: average_over_days(
                         group[[feature, "data", _("denominazione_regione")]],
-                        ["data", "denominazione_regione"],
+                        ["data", _("denominazione_regione")],
                         avg_days,
                     )
                 )
